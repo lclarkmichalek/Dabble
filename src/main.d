0f9bc9e03fd08e104450f5fe74c29e0a381b8e19
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2012-02-25 13:54:15 +0000

    Check for .dabble.conf aswell as .dabble

--- src/main.d
+++ src/main.d
@@ -180,7 +180,8 @@ string find_root_dir() {
     string find_root_prime(string checking) {
         if (checking == "/")
             return "";
-        if (exists(buildPath(checking, ".dabble")) && isDir(buildPath(checking, ".dabble")))
+        if (exists(buildPath(checking, ".dabble")) && isDir(buildPath(checking, ".dabble")) ||
+            exists(buildPath(checking, ".dabble.conf") && isFile(buildPath(checking, ".dabble.conf"))))
             return checking;
         else
             return find_root_prime(buildNormalizedPath(checking, ".."));
