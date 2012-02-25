b64939af81a622cc267498591ed57e5df95306cf
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2012-02-25 14:02:01 +0000

    Moved config intialization from main.d into config.d

--- src/config.d
+++ src/config.d
@@ -87,3 +87,91 @@ string get_user_compile_flags(IniData data) {
 string get_user_link_flags(IniData data) {
     return get(data, "flags", "link");
 }
+
+IniData get_config() {
+    string root_dir = find_root_dir();
+    bool root_found = root_dir != "";
+
+    if (!root_found) {
+        root_dir = guess_root_dir();
+        if (!dot_dabble_exists(root_dir)) {
+            writeln("Creating new .dabble directory in ", root_dir);
+            init_dot_dabble(root_dir);
+        }
+        if (!dabble_conf_exists(root_dir)) {
+            writeln("Creating new .dabble.conf file in ", root_dir);
+            init_dabble_conf(root_dir);
+        }
+    }
+    IniData config = get_dabble_conf(root_dir);
+    config["internal"]["root_dir"] = root_dir;
+    
+    string src_dir;
+    if (get(config, "core", "src_dir") == "") {
+        src_dir = find_src_dir(root_dir);
+        set(config, "core", "src_dir", relativePath(src_dir, root_dir));
+    } else
+        src_dir = absolutePath(get(config, "core", "src_dir"), root_dir);
+    config["internal"]["src_dir"] = src_dir;
+    return config;
+}
+
+string find_root_dir() {
+    string find_root_prime(string checking) {
+        if (checking == "/")
+            return "";
+        if (exists(buildPath(checking, ".dabble")) &&
+            isDir(buildPath(checking, ".dabble")) ||
+            exists(buildPath(checking, ".dabble.conf")) &&
+            isFile(buildPath(checking, ".dabble.conf")))
+            return checking;
+        else
+            return find_root_prime(buildNormalizedPath(checking, ".."));
+    }
+    return absolutePath(find_root_prime(getcwd()));
+}
+
+string guess_root_dir() {
+    int[string] grade_roots(string checking, int[string] grades) {
+        if (checking == "/")
+            return grades;
+
+        int score = 0;
+        // check for src dir
+        string src_path = buildPath(checking, "src");
+        if (exists(src_path) && isDir(src_path))
+            score += 10;
+        else {
+            // Check for project dir (common to have src in there) 
+            src_path = buildPath(checking, baseName(checking));
+            if (exists(src_path) && isDir(src_path))
+                score += 5;
+            src_path = checking;
+        }
+        auto src_files = dirEntries(src_path, SpanMode.shallow);
+        foreach(string filename; src_files) {
+            if (extension(filename) == ".d")
+                score++;
+        }
+        grades[checking] = score;
+        return grade_roots(buildNormalizedPath(checking, ".."), grades);
+    }
+    auto max_grade = -1;
+    auto max_name = "";
+    foreach(name, grade; grade_roots(getcwd, ["": -1])) {
+        if (grade > max_grade) {
+            max_grade = grade;
+            max_name = name;
+        }
+    }
+    return absolutePath(max_name);
+}
+
+
+string find_src_dir(string root) {
+    auto src = buildPath(root, "src");
+    if (exists(src) && isDir(src))
+        return src;
+    else
+        return root;
+}
